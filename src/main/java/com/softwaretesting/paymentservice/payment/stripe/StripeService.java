e27328af77f8c52602b70a091dedc5201b362a44
Mohammadamin Shahimian <mohammadamin.shahimian@drei.com>
2022-02-28 22:18:12 +0100

    add chargeCard to Stripe service

--- src/main/java/com/softwaretesting/paymentservice/payment/stripe/StripeService.java
+++ src/main/java/com/softwaretesting/paymentservice/payment/stripe/StripeService.java
@@ -3,9 +3,13 @@ package com.softwaretesting.paymentservice.payment.stripe;
 import com.softwaretesting.paymentservice.payment.CardPaymentCharge;
 import com.softwaretesting.paymentservice.payment.CardPaymentCharger;
 import com.softwaretesting.paymentservice.payment.Currency;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
 import com.stripe.net.RequestOptions;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StripeService implements CardPaymentCharger {
 	private final RequestOptions requestOptions = RequestOptions.builder().setApiKey("sk_test_4eC39HqLyjWDarjtT1zdp7dc")
@@ -13,6 +17,17 @@ public class StripeService implements CardPaymentCharger {
 	
 	@Override public CardPaymentCharge chargeCard(String cardSource, BigDecimal amount, Currency currency,
 			String description) {
-		return null;
+		Map<String, Object> params = new HashMap<>();
+		params.put("amount", amount);
+		params.put("currency", currency);
+		params.put("source", cardSource);
+		params.put("description", description);
+		
+		try {
+			Charge charge = Charge.create(params,requestOptions);
+			return new CardPaymentCharge(charge.getPaid());
+		} catch (StripeException e) {
+			throw new IllegalStateException("can not make stripe charge",e);
+		}
 	}
 }
