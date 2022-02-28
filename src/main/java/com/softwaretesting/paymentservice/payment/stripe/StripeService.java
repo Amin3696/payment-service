7477876196d9054d0b1e77f5a48724dd3e152e87
Mohammadamin Shahimian <mohammadamin.shahimian@drei.com>
2022-02-28 17:21:02 +0100

    add Stripe service

--- src/main/java/com/softwaretesting/paymentservice/payment/stripe/StripeService.java
+++ src/main/java/com/softwaretesting/paymentservice/payment/stripe/StripeService.java
@@ -1,4 +1,18 @@
 package com.softwaretesting.paymentservice.payment.stripe;
 
-public class StripeService {
+import com.softwaretesting.paymentservice.payment.CardPaymentCharge;
+import com.softwaretesting.paymentservice.payment.CardPaymentCharger;
+import com.softwaretesting.paymentservice.payment.Currency;
+import com.stripe.net.RequestOptions;
+
+import java.math.BigDecimal;
+
+public class StripeService implements CardPaymentCharger {
+	private final RequestOptions requestOptions = RequestOptions.builder().setApiKey("sk_test_4eC39HqLyjWDarjtT1zdp7dc")
+			.build();
+	
+	@Override public CardPaymentCharge chargeCard(String cardSource, BigDecimal amount, Currency currency,
+			String description) {
+		return null;
+	}
 }
